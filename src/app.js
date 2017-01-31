b42b9d6edf57cb16d84486607eead146de0be413
Peter White <pwhitebe@gmail.com>
2017-01-31 13:54:00 -0500

    Updating to support HTTP and redirect HTTPS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+httpsPort = 4400;
+httpPort = 9090;
 var express = require('express'),
     app = express();
 
@@ -10,32 +12,42 @@ var express = require('express'),
 
 //configure express further
 var config = require('./server/config/config'); //may pass env later
+var properties = require ('./server/lib/envProperties');
 require('./server/config/express')(app, config);
-//mongoose goes here
-// passport goes here
-// require('./server/config/mongoose')(config);
-
-// require('./server/config/passport')();
-
 require('./server/config/routes.js')(app);
 
+//if server is set to not use SSL, run http on given port
 
+if(properties.USESSL == 'false')
+{
+    app.listen(httpPort);
+    console.log('Express server listening on port ' + httpPort); 
+}
 
-app.listen(9090);
-console.log('Express server listening on port 9090'); 
+//else if server is set to use SSL, run HTTPS on given port and a re-director on http port
 
+else if (properties.USESSL == 'true')
+{
+//HTTPS 
+    var https = require('https'),      // module for https
+        fs =    require('fs');         // required to read certs and keys
 
+    var options = {
+        key:    fs.readFileSync(properties.SSL_KEY),
+        cert:   fs.readFileSync(properties.SSL_CERT),
+        ca:     fs.readFileSync(properties.SSL_BUNDLE),
+        requestCert:        false,
+        rejectUnauthorized: false,
+        };
+    https.createServer(options, app).listen(httpsPort);
+    console.log('HTTPS Express server listening on port ' + httpsPort); 
 
-//HTTPS for PIV 
-// var https = require('https'),      // module for https
-//     fs =    require('fs');         // required to read certs and keys
+    var http = require('http');
+    http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    //res.writeHead(301, { "Location": "https://localhost:4400" });
+    res.end();
+    }).listen(httpPort);
 
-// var options = {
-//     key:    fs.readFileSync('/sec/certs/server-key.pem'),
-//     cert:   fs.readFileSync('/sec/certs/server-cert.pem'),
-//     ca:     [fs.readFileSync('/sec/certs/gd_bundle-g2.crt'),fs.readFileSync('/sec/certs/HHSPIVcachn.pem')],
-//     requestCert:        true,
-//     rejectUnauthorized: false,
-// };
-
-// https.createServer(options, app).listen(4400);
+    console.log('Redirector listening on port ' + httpPort); 
+}
--- src/server/lib/envPropertiesExample.js
+++ src/server/lib/envPropertiesExample.js
@@ -6,7 +6,12 @@ module.exports = {
   mysqluser: '[mysql-user]',
   mysqlpassword: '[mysql-password]',
   mysqldatabase: '[mysql-database]',
-  port: '3306'
+  port: '3306',
   //port : '/tmp/mysql.sock'  <-- for some linux installations
+   USESSL : 'false',
+  SSL_CERT:  '/path/to/server_cert.pem',
+  SSL_KEY:  '/path/to/server_key.pem',
+  SSL_BUNDLE: '/path/to/gd_bundle-g2.crt'
+
 
 };
